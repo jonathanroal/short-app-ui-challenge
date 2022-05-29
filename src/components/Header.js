69e6826f6546bc9465a732f88dccd2003e859521
jonathanroal <jonathan_ro_alfa@outlook.com>
2022-05-28 22:18:41 -0600

    base code

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import Grid from '@mui/material/Grid';
+
+export default function UrlInput() {
+  return (
+    <Box
+      sx={{
+        marginTop: '30px',
+        width: '80%',
+        height: 300,
+        backgroundColor: 'white',
+        borderRadius: '10px',
+        boxShadow: 3,
+      }}
+    >
+
+    </Box>
+  )
+}
---
+++ src/components/UrlInput.js
@@ -0,0 +1,18 @@
+import Box from '@mui/material/Box';
+
+export default function UrlInput() {
+  return (
+    <Box
+      sx={{
+        marginTop: '30px',
+        width: '80%',
+        height: 300,
+        backgroundColor: 'white',
+        borderRadius: '10px',
+        boxShadow: 3,
+      }}
+    >
+
+    </Box>
+  )
+}
